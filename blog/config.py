f0b806c5f23eb61a2351029819152254de6c69d8
radekwarowny <radekwarownydev@gmail.com>
2021-06-16 10:11:16 +0100

    postgres to postgresl in sqlalchemy

--- blog/config.py
+++ blog/config.py
@@ -1,9 +1,13 @@
 import os
+import re
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SQLAlchemy 1.4x compatibility issues
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
