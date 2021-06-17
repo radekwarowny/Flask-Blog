d7a613b9571edb0202cd95ad90c84d9d95c20a6b
radekwarowny <radekwarownydev@gmail.com>
2021-06-17 10:51:20 +0100

    heroku postgres url

--- blog/config.py
+++ blog/config.py
@@ -5,7 +5,7 @@ import re
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLAlchemy 1.4x compatibility issues
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_ROSE_URL')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
