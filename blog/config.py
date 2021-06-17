547db0bc3f1d8e6ce6f19a8c86fe28183785a101
radekwarowny <radekwarownydev@gmail.com>
2021-06-17 11:09:34 +0100

    db changes

--- blog/config.py
+++ blog/config.py
@@ -1,14 +1,11 @@
 import os
-import psycopg2
-
-
+import re
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLAlchemy 1.4x compatibility issues
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI, sslmode='require')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
