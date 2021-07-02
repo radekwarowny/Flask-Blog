f1eb17cd51022e6ba9a21b99f461deea5d52d430
radekwarowny <radekwarownydev@gmail.com>
2021-07-02 23:15:52 +0100

    changed s3 links

--- blog/users/routes.py
+++ blog/users/routes.py
@@ -68,7 +68,7 @@ def account():
         form.email.data = current_user.email
         # image upload to s3
         # try:
-        image_file = 'https://flask-blog-imgs.s3.eu-west-2.amazonaws.com//home/radek/PycharmProjects/pythonProject/Blog/blog/static/profile_pics/' + current_user.image_file
+        image_file = 'https://flask-blog-imgs.s3.eu-west-2.amazonaws.com//app/blog/static/profile_pics/' + current_user.image_file
         #     return render_template('account.html', title='Account', image_file=image_file, form=form)
         # except Exception as e:
         #     print(e)
