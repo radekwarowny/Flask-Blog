8470f9924ba54be16efa368da7d1741ab46b6602
radekwarowny <radekwarownydev@gmail.com>
2021-07-03 13:33:17 +0100

    made s3 link an environment variable

--- blog/main/routes.py
+++ blog/main/routes.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Blueprint, render_template, request
 from blog.models import Post
 
@@ -6,9 +8,10 @@ main = Blueprint('main', __name__)
 
 @main.route("/home")
 def home():
+    s3 = os.environ.get('S3')
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    return render_template('home.html', posts=posts, s3=s3)
 
 
 @main.route("/about")
--- blog/users/routes.py
+++ blog/users/routes.py
@@ -67,14 +67,7 @@ def account():
         form.username.data = current_user.username
         form.email.data = current_user.email
         # image upload to s3
-        # try:
         image_file = 'https://flask-blog-imgs.s3.eu-west-2.amazonaws.com//app/blog/static/profile_pics/' + current_user.image_file
-        #     return render_template('account.html', title='Account', image_file=image_file, form=form)
-        # except Exception as e:
-        #     print(e)
-        #     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-        #     return render_template('account.html', title='Account', image_file=image_file, form=form)
-        #image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
         return render_template('account.html', title='Account', image_file=image_file, form=form)
 
 
